2285db1fbf989c70b5a63c3968a7faad4de5bc1e
tayutaedomo <tayutaedomo@gmail.com>
2020-05-20 22:57:58 +0900

    Add headless option

--- scripts/scraping_narou_syuppan.py
+++ scripts/scraping_narou_syuppan.py
@@ -1,25 +1,32 @@
+import os
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-url = 'https://syosetu.com/syuppan/list/'
+if __name__ == '__main__':
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--incognito')
+    driver = webdriver.Chrome(options=options)
 
-browser = webdriver.Chrome()
-browser.get(url)
+    url = 'https://syosetu.com/syuppan/list/'
+    driver.get(url)
 
-item_elem_list = browser.find_elements_by_class_name('p-syuppan-list__item')
+    item_elem_list = driver.find_elements_by_class_name('p-syuppan-list__item')
 
-for item_elem in item_elem_list[:1]:
-    title_elem = item_elem.find_element_by_class_name('p-syuppan-list__title')
-    if title_elem:
-        print(title_elem.text)  # TODO
+    for item_elem in item_elem_list[:1]:
+        title_elem = item_elem.find_element_by_class_name('p-syuppan-list__title')
+        if title_elem:
+            print(title_elem.text)  # TODO
 
-    img_elem = item_elem.find_element_by_tag_name('img')
-    if img_elem:
-        print(img_elem.get_attribute('src'))    # TODO
+        img_elem = item_elem.find_element_by_tag_name('img')
+        if img_elem:
+            print(img_elem.get_attribute('src'))    # TODO
 
-    link_elem = item_elem.find_element_by_tag_name('a')
-    if link_elem:
-        print(link_elem.get_attribute('href'))  # TODO
+        link_elem = item_elem.find_element_by_tag_name('a')
+        if link_elem:
+            print(link_elem.get_attribute('href'))  # TODO
 
-
-browser.quit()
+    # Capture
+    file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'tmp', 'capture.png')
+    driver.save_screenshot(file_path)
 
