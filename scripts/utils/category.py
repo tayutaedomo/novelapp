09bf392fe3222114c9d205cbd46bf799a05cf80a
tayutaedomo <tayutaedomo@gmail.com>
2020-05-30 20:33:50 +0900

    Add new category class

--- scripts/utils/category.py
+++ scripts/utils/category.py
@@ -32,3 +32,38 @@ class NarouCategory:
     def to_text(cls, num):
         return cls.categories_reversed.get(num, 'ノンジャンル〔ノンジャンル〕')
 
+
+class NarouCategory2:
+    categories = {
+        'ハイファンタジー〔ファンタジー〕': 0,
+        'ローファンタジー〔ファンタジー〕': 0,
+        '現実世界〔恋愛〕': 1,
+        '異世界〔恋愛〕': 1,
+        '歴史〔文芸〕': 2,
+        '純文学〔文芸〕': 2,
+        'ヒューマンドラマ〔文芸〕': 2,
+        '推理〔文芸〕': 2,
+        'ホラー〔文芸〕': 2,
+        'アクション〔文芸〕': 2,
+        'コメディー〔文芸〕': 2,
+        'VRゲーム〔SF〕': 3,
+        '宇宙〔SF〕': 3,
+        '空想科学〔SF〕': 3,
+        'パニック〔SF〕': 3,
+        '童話〔その他〕': 4,
+        '詩〔その他〕': 4,
+        'エッセイ〔その他〕': 4,
+        'リプレイ〔その他〕': 4,
+        'その他〔その他〕': 4,
+        'ノンジャンル〔ノンジャンル〕': 120,
+    }
+    categories_reversed = dict(map(reversed, categories.items()))
+
+    @classmethod
+    def to_num(cls, text):
+        return cls.categories.get(text, 120)
+
+    @classmethod
+    def to_text(cls, num):
+        return cls.categories_reversed.get(num, 'ノンジャンル〔ノンジャンル〕')
+
